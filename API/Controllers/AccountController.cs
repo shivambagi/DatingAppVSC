1fa07e8692c16d2e907269cf8248acd468f0cf3d
shivambagi <84464752+shivambagi@users.noreply.github.com>
2022-05-01 12:02:31 +0530

    Video 40 - API - Added RegisterDTO and validation to it

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -7,6 +7,8 @@ using System.Text;
 using System.Threading.Tasks;
 using API.Data;
 using API.Entities;
+using API.DTOs;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {    
@@ -19,14 +21,18 @@ namespace API.Controllers
         }
 
         [HttpPost("register")]
-        public async Task<ActionResult<AppUser>> Register(string username,string password)
+        public async Task<ActionResult<AppUser>> Register(RegisterDTO registerDTO)
         {
-             using (var hmac = new HMACSHA512())
+            if (await UserExists(registerDTO.Username))
+            {
+                return BadRequest("Username is taken");
+            }
+            using (var hmac = new HMACSHA512())
             {
                 var user = new AppUser
                 {
-                    UserName = username,
-                    PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password)),
+                    UserName = registerDTO.Username.ToLower(),
+                    PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDTO.Password)),
                     PasswordSalt = hmac.Key
                 };
 
@@ -34,7 +40,12 @@ namespace API.Controllers
                 await _context.SaveChangesAsync();
 
                 return user;
-            }        
+            }            
+        }
+
+        private async Task<bool> UserExists(string username)
+        {
+            return await _context.Users.AnyAsync(x => x.UserName == username.ToLower());
         }
     }
 }
