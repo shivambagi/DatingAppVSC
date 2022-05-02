7e545a4a4de43e685495623c1cdcf705673deb95
shivambagi <84464752+shivambagi@users.noreply.github.com>
2022-05-02 14:57:50 +0530

    Video 63 - UI - Completing register form with http request in accountservice

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { AccountService } from '../_services/account.service';
 
 @Component({
   selector: 'app-register',
@@ -8,18 +9,23 @@ import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 export class RegisterComponent implements OnInit {
 
   model:any = {};
-  @Input() usersFromHomeComponent: any;
 
   @Output() cancelRegister = new EventEmitter();
 
-  constructor() { }
+  constructor(private accountService: AccountService) { }
 
   ngOnInit(): void {
   }
 
   register(){
-    console.log(this.model);
+    this.accountService.register(this.model).subscribe(response => {
+      console.log(response);
+      this.cancel();
+    },error => {
+      console.log(error);
+    })
   }
+
   cancel(){
     this.cancelRegister.emit(false);
   }
