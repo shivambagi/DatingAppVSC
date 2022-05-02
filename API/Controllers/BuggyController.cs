851b5524c9a04084a4489e6b3e14e85c39f43faf
shivambagi <84464752+shivambagi@users.noreply.github.com>
2022-05-02 20:09:56 +0530

    Video 76 - API - Creating a BuggyController with error methods of different types

---
+++ API/Controllers/BuggyController.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using API.Entities;
+using API.Data;
+
+namespace API.Controllers
+{
+    public class BuggyController : BaseApiController
+    {
+        private readonly DataContext _context;
+        public BuggyController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [Authorize]
+        [HttpGet("auth")]
+        public ActionResult<string> GetSecret()
+        {
+            return "secret text";
+        }
+
+        [HttpGet("not-found")]
+        public ActionResult<AppUser> GetNotFOund()
+        {
+            var thing = _context.Users.Find(-1);
+            if (thing == null)
+            { return NotFound(); }
+
+            return Ok(thing);
+        }
+
+        [HttpGet("server-error")]
+        public ActionResult<string> GetServerError()
+        {
+            var thing = _context.Users.Find(-1);
+            var thingToReturn = thing.ToString();
+
+            return Ok(thing);
+        }
+
+        [HttpGet("bad-request")]
+        public ActionResult<string> GetBadRequest()
+        {
+            
+
+            return BadRequest("bad request returned");
+        }
+    }
+}
--- API/DTOs/RegisterDTO.cs
+++ API/DTOs/RegisterDTO.cs
@@ -11,6 +11,7 @@ namespace API.DTOs
         [Required]
         public string Username { get; set; }
         [Required]
+        [StringLength(8,MinimumLength =2)]
         public string Password { get; set; }
     }
 }
