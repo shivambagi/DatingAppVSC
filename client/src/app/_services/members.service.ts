e1564e7d475529d1098938cadb5d43cfd8dd27a0
shivambagi <84464752+shivambagi@users.noreply.github.com>
2022-05-04 10:49:34 +0530

    Video 105*** - UI - Retrieving List of Users and displaying them

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -5,7 +5,7 @@ import { Member } from '../_models/member';
 
 const httpOptions = {
   headers: new HttpHeaders({
-    Authorization: 'Bearer ' + JSON.parse(localStorage.getItem('user')).token
+    Authorization: 'Bearer ' + JSON.parse(localStorage.getItem('user'))?.token
   })
 };
 
@@ -19,10 +19,22 @@ export class MembersService {
   constructor(private http : HttpClient) { }
 
   getMembers() {
-    return this.http.get<Member[]>(this.baseUrl + 'users',httpOptions);
+    return this.http.get<Member[]>(this.baseUrl + 'users',this.getHttpOption());
   }
 
   getMember(username: string) {
-    return this.http.get<Member>(this.baseUrl + 'users/' + username,httpOptions);
+    return this.http.get<Member>(this.baseUrl + 'users/' + username,this.getHttpOption());
   }
+
+
+  getHttpOption()
+  {
+    const httpOptions={
+      headers:new HttpHeaders({
+        Authorization:'Bearer '+JSON.parse(localStorage.getItem('user')).token 
+      })
+    }
+    return httpOptions;
+  }
+
 }
