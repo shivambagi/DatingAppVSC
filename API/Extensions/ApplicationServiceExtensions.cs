fe4fc0d12beab701a69c3b65e50cd820f801f694
shivambagi <84464752+shivambagi@users.noreply.github.com>
2022-05-04 22:48:00 +0530

    Video 127 - API - Configuring Cloudinary in API

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -16,6 +16,8 @@ namespace API.Extensions
     {
         public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration config)
         {
+            services.Configure<CloudinarySettings>(config.GetSection("CloudinarySettings"));
+            
             services.AddScoped<ITokenService, TokenService>();
             services.AddScoped<IUserRepository, UserRepository>();
 
