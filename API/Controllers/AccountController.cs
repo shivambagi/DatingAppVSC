acc05e45ab9772d7612fde427444a3b7c9aa6ec8
shivambagi <84464752+shivambagi@users.noreply.github.com>
2022-05-06 22:25:07 +0530

    Video 136 - API/UI - Adding the main photo image to navbar

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -53,7 +53,9 @@ namespace API.Controllers
         [HttpPost("login")]
         public async Task<ActionResult<UserDTO>> Login(LoginDTO loginDTO)
         {
-            var user = await _context.Users.SingleOrDefaultAsync(x => x.UserName == loginDTO.Username);
+            var user = await _context.Users
+                    .Include(p => p.Photos)
+                    .SingleOrDefaultAsync(x => x.UserName == loginDTO.Username);
             if (user == null)
             {
                 return Unauthorized("Invalid Username");
@@ -73,7 +75,8 @@ namespace API.Controllers
                 return new UserDTO
                 {
                     Username = user.UserName,
-                    Token = _tokenservice.CreateToken(user)
+                    Token = _tokenservice.CreateToken(user),                    
+                    PhotoUrl = user.Photos.FirstOrDefault(x => x.IsMain)?.Url
                 };
             }
         }
