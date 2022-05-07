585c486de6adf98930efc5c2c9933b0cc44879ca
shivambagi <84464752+shivambagi@users.noreply.github.com>
2022-05-07 22:10:53 +0530

    Video 155 - API - Adding helper classes for pagination

---
+++ API/Helpers/UserParams.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Helpers
+{
+    public class UserParams
+    {
+        private const int MaxPageSize = 50;
+
+        public int PageNumber { get; set; } = 1;
+        private int _pagesize = 10;
+
+        public int PageSize
+        {
+            get => _pagesize;
+            set => _pagesize = (value > MaxPageSize) ? MaxPageSize : value;
+        }
+    }
+}
