5e754c173aa393f4d5a7c12125d41f8795b825e6
shivambagi <84464752+shivambagi@users.noreply.github.com>
2022-05-08 21:50:23 +0530

    Video 168 - UI - Restoring the caching for members

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { map, of } from 'rxjs';
+//import { join } from 'path';
+import { map, of, pipe } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { Member } from '../_models/member';
 import { PaginatedResult } from '../_models/pagination';
@@ -19,11 +20,18 @@ export class MembersService {
 
   baseUrl = environment.apiUrl;
   members: Member[] = [];
+  memberCache = new Map();
 
 
   constructor(private http : HttpClient) { }
 
   getMembers(userParams: UserParams) {
+    var response = this.memberCache.get(Object.values(userParams).join('-'));
+    if(response){
+      return of(response);
+    }
+
+
     let params = this.getPaginationHeaders(userParams.pageNumber,userParams.pageSize);
 
     params = params.append('minAge',userParams.minAge.toString());
@@ -31,7 +39,11 @@ export class MembersService {
     params = params.append('gender',userParams.gender);
     params = params.append('orderBy',userParams.orderBy);
 
-    return this.getPaginatedResult<Member[]>(this.baseUrl + 'users',params);
+    return this.getPaginatedResult<Member[]>(this.baseUrl + 'users',params)
+      .pipe(map(response => {
+        this.memberCache.set(Object.values(userParams).join('-'),response);
+        return response;
+      }))
   }
 
   private getPaginatedResult<T>(url, params) {
