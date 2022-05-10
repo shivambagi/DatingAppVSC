349c3f3e4593f788b49094f94d0ff8b8940b0b72
shivambagi <84464752+shivambagi@users.noreply.github.com>
2022-05-10 11:36:30 +0530

    Video 199 - UI - Deleting messages on client

--- client/src/app/messages/messages.component.ts
+++ client/src/app/messages/messages.component.ts
@@ -32,6 +32,12 @@ export class MessagesComponent implements OnInit {
     })
   }
 
+  deleteMessage(id: number){
+    this.messageService.deleteMessage(id).subscribe(() => {
+      this.messages.splice(this.messages.findIndex(m => m.id === id),1);
+    })
+  }
+
   pageChanged(event:any){
     console.log(this.pagination.currentPage);
     if(this.pageNumber !== event.page){
