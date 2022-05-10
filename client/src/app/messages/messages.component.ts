78337aae5842d7e64acd13b7db682f2813bd5b08
shivambagi <84464752+shivambagi@users.noreply.github.com>
2022-05-10 10:36:16 +0530

    Video 197 - UI - Fixing the photo wierdness on messages(Pagination not working on Messages Component)

--- client/src/app/messages/messages.component.ts
+++ client/src/app/messages/messages.component.ts
@@ -11,9 +11,10 @@ import { MessageService } from '../_services/message.service';
 export class MessagesComponent implements OnInit {
   messages: Message[];
   pagination: Pagination;
-  container = 'Outbox';
+  container = 'Unread';
   pageNumber = 1;
   pageSize = 5;
+  loading = false;
 
   constructor(private messageService: MessageService) { }
 
@@ -22,13 +23,17 @@ export class MessagesComponent implements OnInit {
   }
 
   loadMessages(){
+    this.loading = true;
     this.messageService.getMessages(this.pageNumber,this.pageSize,this.container).subscribe(response => {
       this.messages = response.result;
       this.pagination = response.pagination;
+      this.loading = false;
+      console.log(this.pagination.currentPage);
     })
   }
 
   pageChanged(event:any){
+    console.log(this.pagination.currentPage);
     if(this.pageNumber !== event.page){
       this.pagination = event.page;
       this.loadMessages();
